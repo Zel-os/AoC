e2f4f23b4449852aa8a51967082e48a3838dac30
Jake Montgomery <jmont@sonic.net>
2016-12-02 13:43:53 -0800

    part 2

---
+++ AoC1.py
@@ -0,0 +1,34 @@
+inputdata = "L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4"
+direction = 0  # 0=N  1=E  2=S  3=W
+x, y = 0, 0
+s = x, y
+path = set()
+path.add(s)
+
+parsedinput = inputdata.split(", ")
+
+for nextinput in parsedinput:
+    turn, dist = nextinput[:1], int(nextinput[1:])
+    if turn[0] == "L":
+        direction = (direction - 1) % 4
+    else:
+        direction = (direction + 1) % 4
+
+    for i in range(0, dist):
+        if direction == 0:
+            y += 1
+        elif direction == 1:
+            x += 1
+        elif direction == 2:
+            y -= 1
+        elif direction == 3:
+            x -= 1
+        s = x,y
+        if s in path:
+            print("Match at ", s, "(",abs(x) + abs(y),")")
+            exit()
+        path.add(s)
+
+finaldist = abs(x) + abs(y)
+print (x, y, finaldist)
+

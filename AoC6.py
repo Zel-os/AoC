a9e56cddd0cbb0ade3e1ca6d8a2adfda497847a5
Jake Montgomery <jmont@sonic.net>
2016-12-08 02:12:36 -0800

    Day 6, cleaned

--- AoC6.py
+++ AoC6.py
@@ -1,12 +1,9 @@
 inputdata = open("input.txt").readlines()
-columns = zip(*inputdata)
 
-msg = ''
-for col in columns:
-    lmax = 0;  lmin = 99;  letter = ''
+for col in zip(*inputdata):
+    lmax = 0;  lmin = 99;
     for thing in sorted(set(col)):
         if ''.join(col).count(thing) < lmin:  # > lmax
             lmin = ''.join(col).count(thing)  # lmax =
             letter = thing
-    msg += letter
-print(msg)
+    print(letter, end='')

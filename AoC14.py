b6aed92854e69e5018b371cb0f820cd972ef161d
Jake Montgomery <jmont@sonic.net>
2016-12-16 18:34:33 -0800

    Day 14, clearer variable names.

--- AoC14.py
+++ AoC14.py
@@ -8,23 +8,23 @@ def stretch(shash):
 
 
 salt = "ngcjuoqr"
-keytest = []  # [index, testvalue, testhash]
+possible_keys = []  # [index, testvalue, testhash]
 keys = []
 index = 0
 while len(keys) < 74:
-    test = md5((salt+str(index)).encode('utf-8')).hexdigest()
+    current_hash = md5((salt+str(index)).encode('utf-8')).hexdigest()
     test = stretch(test)
-    for testindex, testval, testhash in keytest:
+    for testindex, testval in possible_keys:
         if index > (testindex + 1000):
-            keytest = [x for x in keytest if x[0] != testindex]
+            possible_keys = [x for x in possible_keys if x[0] != testindex]
             continue
-        if test.find(testval) != -1:
+        if current_hash.find(testval) != -1:
             keys.append(testindex)  # [testhash, testindex, index])
-            keytest = [x for x in keytest if x[0] != testindex]
-            print("matched hash",testindex,"with",test)
-    for i in range(0, len(test)-2):
-        if test[i] == test[i+1] and test[i] == test[i+2]:
-            keytest.append([index, test[i]*5, test])
+            possible_keys = [x for x in possible_keys if x[0] != testindex]
+            print("matched hash", testindex, "with", current_hash)
+    for i in range(0, len(current_hash)-2):
+        if current_hash[i] == current_hash[i+1] and current_hash[i] == current_hash[i+2]:
+            possible_keys.append([index, current_hash[i]*5])
             break
     index += 1
 print("64th key at index:", sorted(keys)[63])
